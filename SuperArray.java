ff37f7b148b050c93bc5d59fab4529e9988aaf85
zlin20@stuy.edu <zlin20@stuy.edu>
2020-11-06 12:41:01 -0500

    tested equals() and it worked

--- SuperArray.java
+++ SuperArray.java
@@ -139,4 +139,16 @@ public class SuperArray{
     return position;
   }
 
+  public boolean equals(SuperArray other){
+    if(size()!=other.size()){
+      return false;
+    }
+    for(int i = 0; i < size(); i ++){
+      if(data[i]!=other.get(i)){
+        return false;
+      }
+    }
+    return true;
+  }
+
   }
