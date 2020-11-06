a7ae632329df42ded0792f463d8c06c47b519298
zlin20@stuy.edu <zlin20@stuy.edu>
2020-11-06 12:07:19 -0500

    did findOverLap, tested it with the shorter array first to make sure it worked for that as well

--- Demo.java
+++ Demo.java
@@ -7,8 +7,26 @@ public class Demo{
       }
     }
     }
+    public static SuperArray findOverlap(SuperArray a, SuperArray b){
+      SuperArray output = new SuperArray();
+      for(int i = 0; i < a.size(); i ++){
+          if(b.contains(a.get(i))==true){
+            output.add(a.get(i));
+          }
+      }
+      removeDuplicates(output);
+      return output;
+    }
 
   public static void main(String[]args){
+    SuperArray a = new SuperArray();
+    a.add("9");   a.add("1");     a.add("2");     a.add("2");
+    a.add("3");    a.add("4");
+    SuperArray b = new SuperArray();
+    b.add("0");   b.add("4");     b.add("2");     b.add("2");
+    b.add("9");
+
+    System.out.println(findOverlap(a,b));
     SuperArray words = new SuperArray();
     //grouped to save vertical space
     words.add("kani");   words.add("uni");     words.add("ebi");     words.add("una");
